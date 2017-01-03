97fa515875caec37107c11264fa64b6c5a539f8b
Sérgio Motta <sergio.motta@hyperlinks.com.br>
2017-01-03 01:03:41 -0200

    first version of Calendar of a Lifetime

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    header  = require('gulp-header'),
+    rename = require('gulp-rename'),
+    cssnano = require('gulp-cssnano'),
+    sourcemaps = require('gulp-sourcemaps'),
+    package = require('./package.json');
+
+
+var banner = [
+  '/*!\n' +
+  ' * <%= package.name %>\n' +
+  ' * <%= package.title %>\n' +
+  ' * <%= package.url %>\n' +
+  ' * @author <%= package.author %>\n' +
+  ' * @version <%= package.version %>\n' +
+  ' * Copyright ' + new Date().getFullYear() + '. <%= package.license %> licensed.\n' +
+  ' */',
+  '\n'
+].join('');
+
+gulp.task('css', function () {
+    return gulp.src('scss/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 4 version'))
+    .pipe(gulp.dest('css'))
+    .pipe(cssnano())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(header(banner, { package : package }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('js',function(){
+  gulp.src('js/scripts.js')
+    .pipe(sourcemaps.init())
+    .pipe(header(banner, { package : package }))
+    .pipe(gulp.dest('js'))
+    .pipe(uglify())
+    .pipe(header(banner, { package : package }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('js'))
+    .pipe(browserSync.reload({stream:true, once: true}));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init(null, {
+        server: {
+            baseDir: "."
+        }
+    });
+});
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('default', ['css', 'js', 'browser-sync'], function () {
+    gulp.watch("scss/*.scss", ['css']);
+    gulp.watch("js/*.js", ['js']);
+    gulp.watch("*.html", ['bs-reload']);
+});
---
+++ js/scripts.js
@@ -0,0 +1,75 @@
+/*!
+ * lifetime-calendar
+ * 
+ * 
+ * @author 
+ * @version 1.0.0
+ * Copyright 2017. ISC licensed.
+ */
+/*!
+ * lifetime-calendar
+ * 
+ * 
+ * @author 
+ * @version 1.0.0
+ * Copyright 2017. ISC licensed.
+ */
+/*!
+ * lifetime-calendar
+ * 
+ * 
+ * @author 
+ * @version 1.0.0
+ * Copyright 2017. ISC licensed.
+ */
+/*!
+ * lifetime-calendar
+ * 
+ * 
+ * @author 
+ * @version 1.0.0
+ * Copyright 2017. ISC licensed.
+ */
+/*!
+ * lifetime-calendar
+ * 
+ * 
+ * @author 
+ * @version 1.0.0
+ * Copyright 2017. ISC licensed.
+ */
+/*!
+ * lifetime-calendar
+ * 
+ * 
+ * @author 
+ * @version 1.0.0
+ * Copyright 2017. ISC licensed.
+ */
+/*!
+ * lifetime-calendar
+ * 
+ * 
+ * @author 
+ * @version 1.0.0
+ * Copyright 2017. ISC licensed.
+ */
+/*!
+ * lifetime-calendar
+ * 
+ * 
+ * @author 
+ * @version 1.0.0
+ * Copyright 2017. ISC licensed.
+ */
+$(document).ready(function(){
+   $('.nav-tabs a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+    $('#lifetime').hide();
+    $('#see').click(function(e){
+        e.preventDefault();
+        $('#lifetime').show();
+    }); 
+});
